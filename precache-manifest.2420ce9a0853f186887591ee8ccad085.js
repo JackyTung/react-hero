0cb7ce4268a5cf8cf5586a8f11775d2898e996f2
JackyTung <jackytung8085@gmail.com>
2020-07-28 16:14:19 +0000

    deploy to Github pages

---
+++ precache-manifest.2420ce9a0853f186887591ee8ccad085.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6c464a5ddd50e244a0d4dff7e21db70",
+    "url": "/react-hero/index.html"
+  },
+  {
+    "revision": "0b9f6675beff19e5bbbe",
+    "url": "/react-hero/static/js/2.88b323c0.chunk.js"
+  },
+  {
+    "revision": "13637cfe8b55142f3e1c3fdf742d49b5",
+    "url": "/react-hero/static/js/2.88b323c0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "460d5138a14eb2e4fe10",
+    "url": "/react-hero/static/js/3.76fa5551.chunk.js"
+  },
+  {
+    "revision": "a45a261e4bdb3de9eb59",
+    "url": "/react-hero/static/js/4.dd9ad9c3.chunk.js"
+  },
+  {
+    "revision": "6fac7a4e6a0a78bbeda8",
+    "url": "/react-hero/static/js/main.172559bb.chunk.js"
+  },
+  {
+    "revision": "b056bf7db7a039c4c7db",
+    "url": "/react-hero/static/js/runtime-main.61c00824.js"
+  }
+]);
